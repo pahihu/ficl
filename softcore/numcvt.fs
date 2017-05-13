c46c0412b308888a393862784b4cb5013e4aac70
pahihu <pahia@t-online.hu>
2017-05-13 10:52:50 +0200

    fixed d#,h#

--- softcore/numcvt.fs
+++ softcore/numcvt.fs
@@ -5,16 +5,15 @@ ANEW -numcvt
 \ number conversion
 DECIMAL
 
-: BASE# ( n "number" -- n ) \ cvt next word in given base
-   base @ >r
-      base !  bl word count
-      ?number 0= IF  abort  THEN
-      state @ IF
-         dpl 0< IF    postpone  literal
-                ELSE  postpone 2literal
-                THEN
-      THEN
-   r> base ! ;
+\ : NUMBER? ( ca u -- d 2 | n 1 | 0 )
+\    ?number dup IF  drop  2 dpl @ 0< +  THEN ;
+
+\ NB. ?number compiles/interprets and reports a flag
+: BASE# ( n "number" -- n | ) \ cvt next word in given base
+   base @ >r base !
+      parse-word ?number ( interp: d tf | n tf | 0 ) ( comp: ff )
+   r> base !
+   0= IF  abort  THEN ;
 
 : D# ( "number" -- n ) 10 base# ; IMMEDIATE
 : H# ( "number" -- n ) 16 base# ; IMMEDIATE
