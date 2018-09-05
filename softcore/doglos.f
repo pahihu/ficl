2f24fa986199b7ee41249ba78f0898e75dca1291
Andras Pahi <pahia@t-online.hu>
2018-09-05 12:46:32 +0200

    added GPL glossary generator

---
+++ softcore/doglos.f
@@ -0,0 +1,32 @@
+( FiCL softcore glossary generation -- 5sep18ap )
+INCLUDE glosgen.f
+NEWGLOS
+MAKEGLOS softcore.fr
+MAKEGLOS ifbrack.fr
+MAKEGLOS prefix.fr
+MAKEGLOS ficl.fr
+MAKEGLOS jhlocal.fr
+MAKEGLOS marker.fr
+MAKEGLOS mini-oof.fs
+MAKEGLOS ficllocal.fr
+MAKEGLOS fileaccess.fr
+MAKEGLOS header.fs
+MAKEGLOS srcline.fs
+MAKEGLOS tester.fr
+MAKEGLOS toolbelt.fr
+MAKEGLOS switch.fr
+MAKEGLOS struct.fth
+MAKEGLOS sfcompat.fs
+MAKEGLOS bitwise.fs
+MAKEGLOS circ-pad.fs
+MAKEGLOS missing.fs
+MAKEGLOS floats.fs
+MAKEGLOS dshift.fs
+MAKEGLOS cond.fs
+MAKEGLOS systack.fs
+MAKEGLOS strarray.fs
+MAKEGLOS strconst.f
+MAKEGLOS numcvt.fs
+MAKEGLOS defer.fth
+MAKEGLOS words.fs
+WRITEGLOS ficl.glo
