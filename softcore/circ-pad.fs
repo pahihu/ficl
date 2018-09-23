3bb95f623ff2144fb9f25c879aadc316fa88398c
pahihu <pahia@t-online.hu>
2018-09-23 14:00:53 +0200

    added >BUFFER, >HEAP, S" buffers

--- softcore/circ-pad.fs
+++ softcore/circ-pad.fs
@@ -1,36 +1,69 @@
 \ Circular PADs.
 \ FiCL number conversion and EMIT clashes.
 
-.( loading CIRCULAR PAD ) cr
+.( loading CIRCULAR PAD ) CR
 
 \ ANEW -circ-pad
 
 S" /PAD" ENVIRONMENT? DROP CONSTANT /PAD
 
-hide
+HIDE
 
-  7 CONSTANT #SLOTS	\ number of circular PADs, should be 2^n-1
-    CREATE #PAD 0 ,	\ current PAD index
+  7 CONSTANT #SLOTS	   \ no. of circular buffs, should be 2^n-1
+    CREATE #PAD  0 ,	   \ current PAD index
+    CREATE #SBUF 0 ,    \ current S" buffer index
 
-CREATE pad-area #SLOTS /PAD * allot
+CREATE 'PAD    /PAD CHARS  #SLOTS 1+ *  ALLOT
+CREATE 'SBUF   /PAD CHARS  #SLOTS 1+ *  ALLOT        ( S" buffer)
 
-set-current
+: +SBUF ( -- ca )
+\G Next S" buffer.
+   #SBUF @  1+ #SLOTS AND  DUP #SBUF !
+   /PAD CHARS *  'SBUF + ;
+
+SET-CURRENT
 
 -WARNING
 
 : PAD ( -- caddr )
 \G Return current PAD address <caddr>.
-   pad-area  #pad @ /PAD * + ;
+   /PAD CHARS  #PAD @  *  'PAD + ;
    
 +WARNING
 
 : +PAD ( -- caddr )
 \G Change to next PAD address <caddr>.
-   #pad @  1+ #SLOTS and  #pad !
-   pad ;
+   #PAD @  1+ #SLOTS AND  #PAD !
+   PAD ;
+
+: >BUFFER ( ca u buf -- buf u )
+\G Move string to buffer. Returns buffer address and string
+\G length.
+   SWAP 2DUP 2>R  MOVE  2R> ;
 
 : >PAD ( caddr1 u -- caddr2 u )
 \G Move <caddr>/<u> to next PAD area, return new <caddr2>/<u>.
-   +pad swap dup >r move  pad r> ;
+   +PAD  >BUFFER ;
 
-previous
+: >HEAP ( ca u -- addr u )
+\G Move string <ca/u> to heap.
+   DUP CHARS SAFE-ALLOCATE  >BUFFER ;
+
+
+-WARNING
+
+: S" ( "ccc" -- )
+\G Return the double quote delimited string.
+\G Supports unlimited number of temporary numbers.
+   [CHAR] " PARSE  +SBUF >BUFFER
+   STATE @
+   IF   POSTPONE SLITERAL
+   ELSE >HEAP
+   THEN ; IMMEDIATE
+
++WARNING
+
+
+PREVIOUS
+
+\ vim:ts=3:sw=3:et
