39bb4bef8aa4fee75076bfa13e5581b9c2d7b7d5
pahihu <pahihu@gmail.com>
2021-05-30 14:01:27 +0200

    added glossary generator

---
+++ softcore/ficlglos.f
@@ -0,0 +1,45 @@
+( Glossary generator ----------------------------- 30may2021ap )
+
+include glosgen.f
+
+newglos
+
+makeglos softcore.fr
+makeglos ifbrack.fr
+makeglos prefix.fr
+makeglos ficl.fr
+makeglos jhlocal.fr
+makeglos marker.fr
+makeglos mini-oof.fs
+makeglos ficllocal.fr
+makeglos fileaccess.fr
+makeglos chkredef.fs
+makeglos header.fs
+makeglos srcline.fs
+makeglos toolbelt.fr
+makeglos switch.fr
+makeglos struct.fth
+makeglos sfcompat.fs
+makeglos bitwise.fs
+makeglos circ-pad.fs
+makeglos missing.fs
+makeglos clf.fs
+makeglos floats.fs
+makeglos tester.fr 
+makeglos dshift.fs
+makeglos cond.fs
+makeglos systack.fs
+makeglos strarray.fs
+makeglos strops.fs
+makeglos strconst.f
+makeglos numcvt.fs
+makeglos defer.fth
+makeglos timedate.fs
+makeglos tofcompat.fs
+makeglos ofcompat.fs
+makeglos lose.fs
+makeglos comus.fs
+makeglos tbelt2k.fs
+makeglos words.fs
+
+writeglos ficl.glo
