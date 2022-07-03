8d62f2da88150d7fb2f218e34daa62b636adf087
pahihu <pahihu@gmail.com>
2022-07-03 17:40:25 +0200

    added glossary docs to strarray, switch

--- softcore/strarray.fs
+++ softcore/strarray.fs
@@ -12,12 +12,17 @@
 
 
 : STRING-ARRAY ( -- )   0 ;
+\G Begin definition of a literal string array.
 
 : $" ( i*caI n "string" -- [i+1]*caI [n+1] )
+\G Define a string item.
    [CHAR] " WORD COUNT  HERE >R  DUP C, STRING,
    R> SWAP 1+ ;
 
 : END-STRING-ARRAY ( i*caddrI n "name" -- )
+\G Close the definition of string array `name'.
+\G Executing n `name' will return the nth string in the array
+\G as caddr/u.
    CREATE DUP 1- , 0 ?DO , LOOP
    DOES>  ( # addr -- caddr u )
      @+ ( # addr2 n) ROT - CELLS+ @ COUNT ;
