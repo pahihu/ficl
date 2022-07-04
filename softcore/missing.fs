a58c0ea623c781ee75473d1d51ad9c8eb80cffd5
pahihu <pahihu@gmail.com>
2022-07-04 10:36:23 +0200

    added glossary docs to missing

--- softcore/missing.fs
+++ softcore/missing.fs
@@ -8,14 +8,22 @@
 decimal
 
 Start-Prefixes
-: %  2 __tempbase ; IMMEDIATE
-: &  8 __tempbase ; IMMEDIATE
+: % ( "%..." -- n )
+\G Binary number prefix.
+   2 __tempbase ; IMMEDIATE
+: & ( "&..." -- n )
+\G Octal number prefix.
+   8 __tempbase ; IMMEDIATE
 
 -WARNING
-: # 10 __tempbase ; IMMEDIATE
+: # ( "#..." -- n )
+\G Decimal number prefix.
+   10 __tempbase ; IMMEDIATE
 +WARNING
 
-: $ 16 __tempbase ; IMMEDIATE
+: $ ( "$..." -- n )
+\G Hexadecimal number prefix.
+   16 __tempbase ; IMMEDIATE
 End-Prefixes
 
 S" FICL-OS" ENVIRONMENT? [IF]
@@ -25,44 +33,81 @@ CONSTANT ficl-os
 [THEN]
 
 
-: DARWIN?   \ -- t|f ; true if Darwin
+: DARWIN?   ( -- ff )
+\G True if Darwin.
    ficl-os 1 =
 ;
 
-: LINUX?    \ -- t|f ; true if Linux
+: LINUX?    ( -- ff )
+\G True if Linux.
    ficl-os 2 =
 ;
 
-: WINNT?    \ -- t|f ; true if Windows
+: WINNT?    ( -- )
+\G True if Windows.
    ficl-os 3 =
 ;
 
 DECIMAL
 
-: DIRECTIVE ( -- )   IMMEDIATE COMPILE-ONLY ;
+: DIRECTIVE ( -- )
+\G Make previous word immediate and compile only.
+   IMMEDIATE COMPILE-ONLY ;
 
-: XXX ( -- ) 1 abort" not implemented" ;
+: XXX ( -- )
+\G Abort with `not implemented' message.
+   1 abort" not implemented" ;
 
-: BINARY 2 BASE ! ;
-: OCTAL	 8 BASE ! ;
-: ALPHA  36 BASE ! ;
+: BINARY ( -- )
+\G Set number base to binary.
+   2 BASE ! ;
+: OCTAL ( -- )
+\G Set number base to octal.
+   8 BASE ! ;
+: ALPHA ( -- )
+\G Set number base to 36.
+   36 BASE ! ;
 
 : DOT ( n base -- )
+\G Display `n' in temporary base `base'.
    BASE @ >R  BASE !  U.  R> BASE ! ;
-: .X ( n -- )  16 DOT ;
-: H. ( n -- )  .X ;
-: .B ( n -- )   2 DOT ;
-: .O ( n -- )   8 DOT ;
-: .D ( n -- )  10 DOT ;
-: S. ( n -- )  DUP 0< IF [CHAR] - EMIT THEN  ABS . ;
-: .BASE ( -- )  BASE @  .D ;
+: .X ( n -- )
+\G Display `n' in hexadecimal.
+   16 DOT ;
+: H. ( n -- )
+\G Display `n' in hexadecimal.
+   .X ;
+: .B ( n -- )
+\G Display `n' in binary.
+   2 DOT ;
+: .O ( n -- )
+\G Display `n' in octal.
+   8 DOT ;
+: .D ( n -- )
+\G Display `n' in decimal.
+   10 DOT ;
+: S. ( n -- )
+\G Display `n' as signed number.
+   DUP 0< IF [CHAR] - EMIT THEN  ABS . ;
+: .BASE ( -- )
+\G Display number base in decimal.
+   BASE @  .D ;
 
-: BLANK ( a u -- ) bl fill ;
-: TEXT ( delim -- ) pad 258 blank  word count pad swap  move ;
+: BLANK ( addr u -- )
+\G Fill `u' bytes from `addr' with BL.
+   bl fill ;
+: TEXT ( delim -- )
+\G Move following text delimited with `delim' to PAD.
+   pad 258 blank  word count pad swap  move ;
 
-: STRING, ( ca n -- )   here over chars allot  swap move ;
-: ," ( "string" -- )   [char] " word count string, ;
-: /STRING ( ca1 u1 n -- ca2 u2 ) \ remove +n chars from sc
+: STRING, ( caddr u -- )
+\G Store string `caddr/u' in dictionary space.
+   here over chars allot  swap move ;
+: ," ( "string" -- )
+\G Store delimited string in dictionary space.
+   [char] " word count string, ;
+: /STRING ( caddr1 u1 +n -- caddr2 u2 )
+\G Remove +n chars from `caddr1/u'.
    swap over - >R + R> ;
 
 ( Character constants ---------------------------------------- )
@@ -147,11 +192,19 @@ DECIMAL
 \G Trim, strip leading and trailing spaces.
    -LEADING  -TRAILING ;
 
-: 3DUP ( a b c -- a b c a b c ) dup 2over rot ;
-: BETWEEN ( n lo hi -- f ) 1+ within ;
-: BOUNDS ( lo count -- hi lo ) over + swap ;
+: 3DUP ( a b c -- a b c a b c )
+\G Duplicate top three data stack items.
+   dup 2over rot ;
+: BETWEEN ( n lo hi -- f )
+\G Returns true if lo <= n <= hi.
+   1+ within ;
+: BOUNDS ( lo count -- hi lo )
+\G Convert lo/count to boundary.
+   over + swap ;
 
-: $DUMP ( ca n -- ca n ) 2dup cr ." [" type ." ]" ;
+: $DUMP ( ca n -- ca n )
+\G Display string on output device as "[string]".
+   2dup cr ." [" type ." ]" ;
 
 : HOLDS ( ca u -- )
 \G Hold string in conversion buffer.
@@ -177,40 +230,96 @@ DECIMAL
    THEN
    + ;
 
-: DABS ( d -- d )   DUP 0< if  DNEGATE  THEN ;
-: (UD.) ( ud -- ca n )   <# #S #> ;
-: (D.) ( d -- ca n )   DUP >R DABS  <# #S R> SIGN #> ;
-: (.)  ( n -- ca n )   S>D (D.) ;
-: (U.) ( n -- ca n )   0 (UD.) ;
-: (U.R) ( u m -- ca #)   >R (U.) R> RJUSTIFY ;
-: U.R ( u m -- )   (U.R) TYPE ;
-: (.R) ( n m -- ca #)   >R (.) R> RJUSTIFY ;
-: .R ( n m -- )   (.R) TYPE ;
+: DABS ( d -- ud )
+\G Convert double cell number to its absolute value.
+   DUP 0< if  DNEGATE  THEN ;
+: (UD.) ( ud -- ca n )
+\G Convert unsigned double number to string.
+   <# #S #> ;
+: (D.) ( d -- ca n )
+\G Convert double number to string.
+   DUP >R DABS  <# #S R> SIGN #> ;
+: (.)  ( n -- ca n )
+\G Convert number to string.
+   S>D (D.) ;
+: (U.) ( u -- ca n )
+\G Convert unsigned number to string.
+   0 (UD.) ;
+: (U.R) ( u m -- ca #)
+\G Convert unsigned number to string in a field right-justified.
+   >R (U.) R> RJUSTIFY ;
+: U.R ( u m -- )
+\G Display unsigned number in a field right-justified.
+   (U.R) TYPE ;
+: (.R) ( n m -- ca #)
+\G Convert number to string in a field right-justified.
+   >R (.) R> RJUSTIFY ;
+: .R ( n m -- )
+\G Display number in a field right-justified.
+   (.R) TYPE ;
 
 -WARNING
-: . ( n -- )   (.) TYPE SPACE ;
+: . ( n -- )
+\G Display number.
+   (.) TYPE SPACE ;
 +WARNING
 
-: UD. ( u -- )   (UD.) TYPE SPACE ;
-: D. ( d -- )   (D.) TYPE SPACE ;
-: (UD.R) ( d m -- ca # )   >R (UD.) R> RJUSTIFY ;
-: UD.R ( ud m -- )   (UD.R) TYPE ;
-: (D.R) ( d m -- ca n )   >R (D.) R> RJUSTIFY ;
-: D.R ( d m -- )   (D.R) TYPE ;
+: UD. ( ud -- )
+\G Display unsigned double number.
+   (UD.) TYPE SPACE ;
+: D. ( d -- )
+\G Display double number.
+   (D.) TYPE SPACE ;
+: (UD.R) ( ud m -- ca # )
+\G Convert unsigned double number to string in a field right-justified.
+   >R (UD.) R> RJUSTIFY ;
+: UD.R ( ud m -- )
+\G Display unsigned double number in a field right-justified.
+   (UD.R) TYPE ;
+: (D.R) ( d m -- ca n )
+\G Convert double number to string in a field right-justified.
+   >R (D.) R> RJUSTIFY ;
+: D.R ( d m -- )
+\G Display double number in a field right-justified.
+   (D.R) TYPE ;
 
-: UMAX ( u1 u2 -- u ) 2dup u< IF swap THEN drop ;
-: UM+ ( u1 u2 -- ud ) over +  dup rot  u< abs ;
-: D+ ( d1 d2 -- d ) rot + >r um+ r> + ;
-: D- ( d1 d2 -- d ) dnegate d+ ;
-: D0= ( d -- f ) or 0= ;
-: D0< ( d -- f ) 0< nip ;
-: D= ( d1 d2 -- f ) d- d0= ;
-: D<> ( d1 d2 -- f ) d= 0= ;
+: UMAX ( u1 u2 -- u )
+\G Return the maximum of two unsigned numbers.
+   2dup u< IF swap THEN drop ;
+: UM+ ( u1 u2 -- ud )
+\G Add two unsigned number producing an unsigned double result.
+   over +  dup rot  u< abs ;
+: D+ ( d1 d2 -- d )
+\G Add two double numbers.
+   rot + >r um+ r> + ;
+: D- ( d1 d2 -- d )
+\G Subtract two double numbers.
+   dnegate d+ ;
+: D0= ( d -- f )
+\G Return true if double number is zero.
+   or 0= ;
+: D0< ( d -- f )
+\G Return true if double number is less-than zero.
+   0< nip ;
+: D= ( d1 d2 -- f )
+\G Return true if two double numbers are equal.
+   d- d0= ;
+: D<> ( d1 d2 -- f )
+\G Returns true if two double numbers are not equal.
+   d= 0= ;
 
-: M+ ( d1 n -- d2 ) s>d d+ ;
-: <= ( n1 n2 -- ff ) > 0= ;
-: >= ( n1 n2 -- ff ) < 0= ;
-: \\ ( -- ) BEGIN  refill 0=  UNTIL ;
+: M+ ( d1 n -- d2 )
+\G Add double and single number producing a double result.
+   s>d d+ ;
+: <= ( n1 n2 -- ff )
+\G Return true if `n1' is less-or-equal to `n2'.
+   > 0= ;
+: >= ( n1 n2 -- ff )
+\G Return true if `n1' is greater-or-equal to `n2'.
+   < 0= ;
+: \\ ( -- )
+\G Skip remaining text in a file.
+   BEGIN  refill 0=  UNTIL ;
 
 \ ANSI colors
 0 CONSTANT black
@@ -235,30 +344,50 @@ WINNT? 0=
 : SET-FG ( color -- ) 30 +  sgr ;
 : SET-BG ( color -- ) 40 +  sgr ;
 : RESET-ATTRS ( -- ) 0 sgr ;
-: AT-XY ( col row -- ) 1+ SWAP 1+ SWAP cup ;
-: HOME ( -- ) 0 0 at-xy ;
-: PAGE ( -- ) home cls ;
+: AT-XY ( col row -- )
+\G Position cursor to col/row.
+   1+ SWAP 1+ SWAP cup ;
+: HOME ( -- )
+\G Position cursor to top left corner.
+   0 0 at-xy ;
+: PAGE ( -- )
+\G Clear page.
+   home cls ;
 
 [ENDIF]
 
-: /TERM ( -- ) 1 PREPTERM ;
-: -TERM ( -- ) 0 PREPTERM ;
+: /TERM ( -- )
+\G Prepare terminal for raw terminal input.
+   1 PREPTERM ;
+: -TERM ( -- )
+\G Turn off raw terminal input.
+   0 PREPTERM ;
 
 \ Array definitions
 : ARRAY ( n "name" -- )
+\G Define an array `name' with n cells.
+\G Usage: name ( n -- addr )
+\G Return the address of the nth cell.
    CREATE CELLS ALLOT
    DOES>  ( n addr1 -- addr2 ) SWAP CELLS+ ;
 
 : CARRAY ( n "name" -- )
+\G Define an array `name' with n chars.
+\G Usage: name ( n -- caddr )
+\G Return the address of the nth char.
    CREATE CHARS ALLOT
    DOES>  ( n addr1 -- addr2 ) SWAP CHARS+ ;
 
-: BUFFER: ( n "name" -- )   CREATE CHARS ALLOT ;
+: BUFFER: ( n "name" -- )
+\G Allot a buffer `name' with `n' chars in dictionary space.
+   CREATE CHARS ALLOT ;
 
 
 
 \ DUMP
-: ?PRINTABLE ( c -- f ) 32 128 within ;
+: ?PRINTABLE ( c -- f )
+\G Return true if char is printable.
+   32 128 within ;
 
 hide
 
@@ -289,6 +418,7 @@ set-current
 -WARNING
 
 : DUMP ( addr u -- )
+\G Dump memory `u' bytes from `addr' in hexadecimal.
 	base @ >r hex
 	>r ( 16*k)  r> 15 + ( 16*k)  bounds DO
 		i dump-line
@@ -299,7 +429,9 @@ set-current
 previous
 
 \ P.Falth, W.Baden
-: .. ( i*x -- )   DEPTH IF  .S  DEPTH 0 DO DROP LOOP  THEN ;
+: .. ( i*x -- )
+\G Display the contents of the data stack and clear the stack.
+   DEPTH IF  .S  DEPTH 0 DO DROP LOOP  THEN ;
 
 \ strings
 : $VARIABLE ( len "name" -- )
@@ -347,23 +479,33 @@ hide
 
 set-current
 
-: SOURCE! ( ca u -- ) source.fs $PLACE ;
-: IN ( -- ) \ include source.fs
+: SOURCE! ( ca u -- )
+\G Set source file name to `ca/u'.
+   source.fs $PLACE ;
+: IN ( -- )
+\G Include current source file.
 	S" include "  PAD $PLACE
 	source.fs $COUNT  PAD $APPEND
 	PAD $COUNT  EVALUATE ;
 
-: EDITED ( ca u -- ) \ edit name
+: EDITED ( ca u -- )
+\G Edit file named `ca/u'.
 	S" system vim " PAD $PLACE  PAD $APPEND
 	PAD $COUNT  EVALUATE ;
 
-: ED ( -- ) \ edit source.fs
+: ED ( -- ) 
+\G Edit current source.
 	source.fs $COUNT  edited ;
 
-: EDIT ( "filename" -- ) bl word count  edited ;
+: EDIT ( "filename" -- )
+\G Edit file named `filename'.
+   bl word count  edited ;
 
-: LS ( -- ) S" system ls" EVALUATE ;
-: RM ( name -- )
+: LS ( -- )
+\G List current directory.
+   S" system ls" EVALUATE ;
+: RM ( "name" -- )
+\G Remove file named `name'.
 	s" system rm " PAD $PLACE
 	bl word count  PAD $APPEND
 	PAD $COUNT  EVALUATE ;
